813649c85ff9f0cdd2a157451a5f824d4f980c5b
Данир <samsmamsam@example.com>
2026-09-15 10:18:05 +0500

    сделали все до 3 задания 5-6пр

---
+++ ProfileCard.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDesignPractice2
+{
+    /// <summary>
+    /// Логика взаимодействия для ProfileCard.xaml
+    /// </summary>
+    public partial class ProfileCard : UserControl
+    {
+        public ProfileCard()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+        }
+
+        public static readonly DependencyProperty AvatarPathProperty =
+            DependencyProperty.Register("AvatarPath", typeof(string), typeof(ProfileCard));
+
+        public static readonly DependencyProperty NameProperty =
+            DependencyProperty.Register("Name", typeof(string), typeof(ProfileCard));
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ProfileCard));
+
+        public string AvatarPath
+        {
+            get { return (string)GetValue(AvatarPathProperty); }
+            set { SetValue(AvatarPathProperty, value); }
+        }
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
+
