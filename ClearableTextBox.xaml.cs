56edcd0589044da51320620f36b63ae9f4c9202a
Данир <samsmamsam@example.com>
2026-09-15 10:57:44 +0500

    главное изменение формы

---
+++ ClearableTextBox.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDesignPractice2
+{
+    /// <summary>
+    /// Логика взаимодействия для ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public static readonly DependencyProperty TextProperty =
+           DependencyProperty.Register("Text", typeof(string), typeof(ClearableTextBox),
+               new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Text = string.Empty;
+        }
+
+    }
+}
